55c9bd06de00d77bd016703d6e4caac21004ad79
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2025-09-11 11:21:21 -0700

    Update task_main.py

--- frontend/task_main.py
+++ frontend/task_main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import datetime
 import requests
 
-API_URL = "http://localhost:8000"
+API_URL = "https://vsqzkfpdmxlnjmopfgde.supabase.co"
 
 def main_screen_tasks():
     selected_date = st.date_input("Enter Date", datetime.today(), label_visibility="collapsed")
