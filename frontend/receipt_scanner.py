fd2189457c4375988b149fc1f2f23d56b861b1b1
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2026-03-28 04:35:22 -0700

    Update receipt_scanner.py

--- frontend/receipt_scanner.py
+++ frontend/receipt_scanner.py
@@ -20,6 +20,7 @@ def scanner():
     st.markdown("Upload a receipt to automatically extract the store, date, and total cost")
 
     uploaded_file = st.file_uploader("Choose a receipt image...", type=["jpg", "jpeg", "png", "pdf", "webp", "heic"])
+    st.image(img, caption="Uploaded Receipt", use_column_width=True)
     if uploaded_file is not None:
         api_key = st.secrets["GEMINI_API_KEY"]
         client = genai.Client(api_key=api_key)
