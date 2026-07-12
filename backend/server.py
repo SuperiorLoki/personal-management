587c9495cc46cabc5f215ce305a6b38b02c6be7e
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2026-07-11 22:43:12 -0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,30 @@
 from urllib.error import HTTPError
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends,status
 from datetime import date
 from backend import db_helper
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 import logging
+import auth
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
 app = FastAPI()
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str 
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 class Expense(BaseModel):
     amount: float
@@ -27,10 +40,30 @@ class DateRange(BaseModel):
     start_date: date
     end_date: date
 
+@app.post("/register", status_code=status.HTTP_201_CREATED)
+def register_user(user: UserCreate):
+    existing_user = db_helper.get_user_by_email(user.email)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email is already registered")
+    hashed_pw = auth.get_password_hash(user.password)
+
+    user_id = db_helper.create_user(user.email, hashed_pw)
+    return {"message": "User created successfully!", "user_id": user_id}
+
+@app.post("/login", response_model=Token)
+def login_user(user: UserLogin):
+    db_user = db_helper.get_user_by_email(user.email)
+
+    if not db_user or not auth.verify_password(user.password, db_user['hashed_password']):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    
+    access_token = auth.create_access_token(data={"sub": db_user['email']})
+    return {"access_token": access_token, "token_type": "bearer"}
+
 #the response model will ensure that we only get a response of what we want that is stated in the Expense class
 @app.get("/expenses/{expense_date}", response_model=List[Expense])
-def get_expense(expense_date: date):
-    expenses = db_helper.fetch_expenses_for_date(expense_date)
+def get_expense(expense_date: date, current_user: dict = Depends(auth.get_current_user)):
+    expenses = db_helper.fetch_expenses_for_date(expense_date, current_user['id'])
 
     if expenses is None:
         raise HTTPException(status_code=500, detail="Failed to retrieve expenses from the database.")
@@ -38,19 +71,19 @@ def get_expense(expense_date: date):
 
 
 @app.post("/expenses/{expense_date}")
-def add_or_update_expenses(expense_date: date, expenses:List[Expense]):
+def add_or_update_expenses(expense_date: date, expenses:List[Expense], current_user: dict = Depends(auth.get_current_user)):
     try:
-        db_helper.delete_expenses_for_date(expense_date)
+        db_helper.delete_expenses_for_date(expense_date, current_user['id'])
         for expense in expenses:
-            db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
+            db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes, current_user['id'])
         return {"message": "Expenses updated successfully"}
     except Exception as e:
-        logger.exception("POST /task failed")
+        logger.exception("POST /tasks failed")
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/analytics/")
-def get_analytics(date_range: DateRange):
-    data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date)
+def get_analytics(date_range: DateRange, current_user: dict = Depends(auth.get_current_user)):
+    data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date, current_user['id'])
     if data is None:
         raise HTTPException(status_code=500, detail="Failed to retrieve summary from the database.")
 
@@ -73,15 +106,15 @@ def get_analytics(date_range: DateRange):
     return breakdown
 
 @app.get("/month_breakdown/")
-def get_analytics_month():
-    data = db_helper.fetch_month()
+def get_analytics_month(current_user: dict = Depends(auth.get_current_user)):
+    data = db_helper.fetch_month(current_user['id'])
     if data is None:
         raise HTTPException(status_code=500, detail="Failed to retrieve data.")
     return data
 
 @app.get("/report/")
-def get_reports():
-    data = db_helper.fetch_all_records()
+def get_reports(current_user: dict = Depends(auth.get_current_user)):
+    data = db_helper.fetch_all_records(current_user['id'])
     if data is None:
         raise HTTPException(status_code=500, detail="Failed to retrieve data.")
     return data
