f46753d420f8c4864ae81ac55d19a5dee868613e
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2026-07-11 22:44:56 -0700

    Update main_page.py

--- frontend/main_page.py
+++ frontend/main_page.py
@@ -5,9 +5,59 @@ import pandas as pd
 
 API_URL = "https://personal-management-1.onrender.com"
 
+if "token" not in st.session_state:
+    st.session_state["token"] = None 
+if "user_email" not in st.session_state:
+    st.session_state["user_email"] = None
+
+def auth_screen():
+    st.title("Welcome to AI Expense Tracker")
+    tab1, tab2 = st.tabs(["Login", "Sign Up"])
+    with tab1:
+        with st.form("Login_form"):
+            email = st.text_input("Email", key="login_email")
+            password = st.text_input("Password", type="password", key="login_password")
+            if st.form_submit_button("Login"):
+                res = requests.post(f"{API_URL}/login", json={"email": email, "password": password})
+                if res.status_code == 200:
+                    st.session_state["token"] = res.json()["access_token"]
+                    st.session_state["user_email"] = email
+                    st.success("Log In Success!")
+                    st.rerun()
+                else:
+                    st.error("Invalid email or password.")
+    with tab2:
+        with st.form("signup_form"):
+            new_email = st.text_input("Email", key="signup_email")
+            new_password = st.text_input("Password", type="password", key="signup_password")
+
+            if st.form_submit_button("Create Account"):
+                res = requests.post(f"{API_URL}/register", json={"email": new_email, "password": new_password})
+
+                if res.status_code == 201:
+                    st.success("Account Created! Please switch to the Login tab to login.")
+                else:
+                    st.error("Failed to create account. That email might already be registered.")
+
+if not st.session_state["token"]:
+    auth_screen()
+
+    st.stop()
+
+with st.sidebar():
+    st.write(f"Logged in as:\n**{st.session_state['user_email']}**")
+    if st.button("Logout"):
+        st.session_state["token"] = None
+        st.session_state["user_email"] = None
+        
+        st.rerun()
+
+
+
 def main_screen():
+        headers = {"Authorization": f"Bearer {st.session_state['token']}"}
         selected_date = st.date_input("Enter Date", datetime.today(), label_visibility="collapsed")
-        response = requests.get(f"{API_URL}/expenses/{selected_date}")
+        response = requests.get(f"{API_URL}/expenses/{selected_date}", headers=headers)
         if response.status_code == 200:
             existing_expenses = response.json()
         else:
@@ -60,7 +110,7 @@ def main_screen():
             submit_button = st.form_submit_button()
             if submit_button:
                 filtered_expenses = [expense for expense in expenses if expense['amount']>0.0]
-                response = requests.post(f"{API_URL}/expenses/{selected_date}", json = filtered_expenses)
+                response = requests.post(f"{API_URL}/expenses/{selected_date}", json = filtered_expenses, headers=headers)
                 #st.write(filtered_expenses)
                 if response.status_code == 200:
                     st.success("Expenses updated successfully.")
