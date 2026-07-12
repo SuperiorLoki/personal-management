eebed06588ad94d03e9ec216713809ac2f70da30
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2026-07-11 23:41:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-from frontend.all_tasks import all_tasks
 from frontend.main_page import main_screen
 from frontend.analytics_ui import analytics
 from frontend.month_analytics import months
 from frontend.receipt_scanner import scanner
 from frontend.report_ui import report
-from frontend.task_main import main_screen_tasks
 #from user_analytics import user_analytics
 
 col_left, col_center, col_right = st.columns([0.001, 150, 0.001])  # center is slightly wider
@@ -41,36 +39,19 @@ from supabase import create_client
 #     st.write(f"Hello, your ID is {user_id}")
 
 
-side_tab = st.sidebar.radio(
-    "Menu Options",
-    ["Expense Tracker", "Planning System"]
-)
+st.title("Expense Tracking System")
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Add/Update", "Analytics", "Month by Month Breakdown", "Report", "Scanner"])
 
-
-
-if side_tab == "Expense Tracker":
-    st.title("Expense Tracking System")
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Add/Update", "Analytics", "Month by Month Breakdown", "Report", "Scanner"])
-
-    with tab1:
-        main_screen()
-    with tab2:
-        analytics()
-    with tab3:
-        months()
-    with tab4:
-        report()
-    with tab5:
-        scanner()
-
-if side_tab == "Planning System":
-    st.title("Planning System")
-    tab1, tab2  = st.tabs(["To-Do List", "Planner"])
-
-    with tab1:
-        main_screen_tasks()
-    with tab2:
-        all_tasks()
+with tab1:
+    main_screen()
+with tab2:
+    analytics()
+with tab3:
+    months()
+with tab4:
+    report()
+with tab5:
+    scanner()
 
     #
 
