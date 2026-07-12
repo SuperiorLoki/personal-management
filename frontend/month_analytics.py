a78b4a925d056ae16aadb03c6ef7deb82b2ecc3f
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2026-07-12 00:01:29 -0700

    Update month_analytics.py

--- frontend/month_analytics.py
+++ frontend/month_analytics.py
@@ -8,7 +8,7 @@ import pandas as pd
 API_URL = "https://personal-management-1.onrender.com"
 
 def months():
-    headers = {"Authorization": f"Bearer {st.session_state['token']}"}
+    headers = {"Authorization": f"Bearer {st.session_state.get('token')}"}
     response = requests.get(f"{API_URL}/month_breakdown/", headers=headers)
     if response.status_code == 200:
         existing_expenses = response.json()
