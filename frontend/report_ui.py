b4e51cf5094a81aefc242fe8dbf29106019bb060
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2026-07-12 00:03:12 -0700

    Update report_ui.py

--- frontend/report_ui.py
+++ frontend/report_ui.py
@@ -5,7 +5,7 @@ import pandas as pd
 API_URL = "https://personal-management-1.onrender.com"
 
 def report():
-    headers = {"Authorization": f"Bearer {st.session_state['token']}"}
+    headers = {"Authorization": f"Bearer {st.session_state.get('token')}"}
     response = requests.get(f"{API_URL}/report/")
     if response.status_code == 200:
         existing_expenses = response.json()
