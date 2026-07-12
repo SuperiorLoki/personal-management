b2ed0553c12d90163ec73afd285442e923c57aee
Hirithik Pranav <110261542+SuperiorLoki@users.noreply.github.com>
2026-07-11 22:53:03 -0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,7 @@ from backend import db_helper
 from typing import List
 from pydantic import BaseModel, EmailStr
 import logging
-import auth
+import backend import auth
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
